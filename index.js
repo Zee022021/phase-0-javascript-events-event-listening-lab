eda0ea24ba8cc187205bf3bf1076e89d1cc416b7
ZiaulhaqHaqyar <fnu.ziaulhaq@flatironschool.com>
2022-02-13 17:41:45 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-function addingEventListener() {
+
+const input = document.getElementById('input');
+
+function addingEventListener(name) {
+    alert('I was clicked');
+    input.addEventListener('click', addingEventListener);
+    
 }
+
